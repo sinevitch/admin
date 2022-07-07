31e1bafdfadbebb4f1b729158082d2dad3c2302c
sinevitch <35930695+sinevitch@users.noreply.github.com>
2022-07-08 00:28:17 +0300

    change

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,4 @@
 module.exports.mongoUrl = 'mongodb+srv://hatabelsite:passwordhatabel@cluster0.t6wgm.mongodb.net/hata-prod?retryWrites=true&w=majority';
-module.exports.sendGridKey = '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';
 module.exports.captchaKey = '6Lc1LMAUAAAAAMtm1Lh6LAq5XzCAyz_hJUmXFUON';
 module.exports.jwtSecret = 'hatabelsite';
 module.exports.cloudApiKey = '872143636867361';
